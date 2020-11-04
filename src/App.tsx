3acbb2ed2e6d88471f7793f36b3768fb76c1bf28
aless10 <alessio.izzo86@gmail.com>
2020-11-04 23:38:03 +0100

    add session and break functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [sessionTime, setSessionTime] = React.useState(25)
+  const [breakTime, setBreakTime] = React.useState(5)
+
+  const onIncrementSession = () => {
+    setSessionTime((prevState => prevState + 1))
+  }
+
+  const onIncrementBreak = () => {
+    setBreakTime((prevState => prevState + 1))
+  }
+
+  const onDecrementSession = () => {
+    setSessionTime((prevState => prevState === 0 ? 0 : prevState - 1))
+  }
+
+  const onDecrementBreak = () => {
+    setBreakTime((prevState => prevState === 0 ? 0 : prevState - 1))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="session">
+        <button onClick={onIncrementSession}>+</button>
+        <span>{sessionTime}</span>
+        <button onClick={onDecrementSession}>-</button>
+      </div>
+      <div id="break">
+        <button onClick={onIncrementBreak}>+</button>
+        <span >{breakTime}</span>
+        <button onClick={onDecrementBreak}>-</button>
+      </div>
     </div>
   );
 }
